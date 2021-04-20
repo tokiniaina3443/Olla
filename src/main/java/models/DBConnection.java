bfae97840ddfc3bb10f25244950f162e9593cc0f
Tokiniaina <randrianarimananatokiniaina47@gmail.com>
2021-04-20 15:57:50 +0300

    commit

--- src/main/java/models/DBConnection.java
+++ src/main/java/models/DBConnection.java
@@ -3,12 +3,13 @@ package models;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
+import java.io.FileReader;
 import java.io.IOException;
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DBConnection 
 {   
@@ -18,11 +19,11 @@ public class DBConnection
         try 
         {
             //online1----------
-            URI dbUri = new URI("postgres://xgcpswxnrykixy:8107cb8ada5af6edbed4d2800855173663016e6656682da6f278d983e96d610a@ec2-52-19-170-215.eu-west-1.compute.amazonaws.com:5432/dfc72ctu54dqo2");
+            /*URI dbUri = new URI("postgres://xgcpswxnrykixy:8107cb8ada5af6edbed4d2800855173663016e6656682da6f278d983e96d610a@ec2-52-19-170-215.eu-west-1.compute.amazonaws.com:5432/dfc72ctu54dqo2");
             String username = dbUri.getUserInfo().split(":")[0];
             String password = dbUri.getUserInfo().split(":")[1];
             String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
-            conn = DriverManager.getConnection(dbUrl, username, password);
+            conn = DriverManager.getConnection(dbUrl, username, password);*/
             
             //online2----------
             /*String url ="jdbc:postgresql://postgresql-26417-0.cloudclusters.net:26417/AppelTelephonique";
@@ -31,14 +32,14 @@ public class DBConnection
             conn = DriverManager.getConnection(url, user, password);*/
             
             //offline---------
-            /*FileReader reader = new FileReader("src\\main\\resources\\db.properties");
+            FileReader reader = new FileReader("src\\main\\resources\\db.properties");
             Properties p = new Properties();
             p.load(reader);
             
             String url = p.getProperty("url");
             String user = p.getProperty("user");
             String password = p.getProperty("password");
-            conn = DriverManager.getConnection(url, user, password);*/
+            conn = DriverManager.getConnection(url, user, password);
      
         } 
         catch (SQLException e) 
